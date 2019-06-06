86caae5f113bd59cc5ce30f342946a66fb93d0e9
Samuel Guo <47992471+guosamuel@users.noreply.github.com>
2019-06-06 12:11:55 -0400

    Completed Seed Data

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,7 +2,7 @@ class Task < ApplicationRecord
   belongs_to :project
 
   validates :project_id, presence: true
-  validates :title, presence: true, uniquenss: true
+  validates :title, presence: true
   validates :description, presence: true
   validates :due_date, presence: true
 end
