6a2675f47576ef22ce302fc83786508a2f5c1b62
Samuel Guo <47992471+guosamuel@users.noreply.github.com>
2019-06-06 10:44:50 -0400

    Completed validations in back end

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,8 @@
 class Task < ApplicationRecord
   belongs_to :project
+
+  validates :project_id, presence: true
+  validates :title, presence: true, uniquenss: true
+  validates :description, presence: true
+  validates :due_date, presence: true
 end
