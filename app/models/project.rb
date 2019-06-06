da6a9efff0bb56ed7ff475eabdf09b8ac38ee15c
github username <github email address>
2019-06-06 10:44:50 -0400

    Completed validations in back end

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,4 +2,9 @@ class Project < ApplicationRecord
   has_many :user_projects
   has_many :users, through: :user_projects
   has_many :tasks
+
+  validates :title, presence: true, uniqueness: true
+  validates :description, presence: true
+  validates :due_date, presence: true
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
 
   has_many :user_projects
   has_many :projects, through: :user_projects
+  has_many :tasks, through: :projects
+
+  validates :username, presence: true, uniqueness: true
+  validates :password_digest, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+
 end
