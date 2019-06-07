dd8e55a0b158070a850f61674c635ad285672984
Samuel Guo <47992471+guosamuel@users.noreply.github.com>
2019-06-07 15:39:41 -0400

    Trying to do redux auth

--- Gemfile
+++ Gemfile
@@ -29,6 +29,10 @@ gem 'bootsnap', '>= 1.1.0', require: false
 gem 'rack-cors'
 
 gem 'active_model_serializers', '~> 0.10.0'
+gem 'dotenv-rails', groups: [:development, :test]
+
+gem 'jwt'
+
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,38 @@
 class ApplicationController < ActionController::API #requests can come from anywhere
 
+  def encode_token(user_id)
+    JWT.encode({user_id: user_id}, ENV['SECRET_TOKEN'])
+  end
 
+  def auth_headers
+    request.headers['Authorization']
+  end
+
+  def decoded_token
+    begin
+      JWT.decode(token, ENV['SECRET_TOKEN'])
+    rescue
+      nil
+    end
+  end
+
+  def current_user
+    begin
+      user_id = decoded_token[0]["user_id"]
+      User.find(user_id)
+    rescue
+      nil
+    end
+  end
+
+  def logged_in
+    !!current_user
+  end
+
+  def authorized
+    render json: {error: "Unauthorized"}
+  end
+  
 end
 
 # typical rails fullstack app -> every single form and request shooooould come from rails
