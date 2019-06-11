ab03f02c5330a066633f0b285c3de6a1de683fd6
github username <github email address>
2019-06-11 18:08:38 -0400

    Got the new project form info to the backend

--- app/controllers/api/v1/projects_controller.rb
+++ app/controllers/api/v1/projects_controller.rb
@@ -5,4 +5,13 @@ class Api::V1::ProjectsController < ApplicationController
     render json: @projects
   end
 
+  def create
+    month = params[:date][0..1]
+    day = params[:date][3..4]
+    year = params[:date][6..9]
+    formatted_date = "#{year}-#{month}-#{day}"
+    new_project = Project.create(title: params[:title], description: params[:description], due_date: formatted_date, percentage: 0)
+    render json: new_project
+  end
+
 end
