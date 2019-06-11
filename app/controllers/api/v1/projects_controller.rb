e845496c660b9f5641d77a1772493629c2680be3
Samuel Guo <47992471+guosamuel@users.noreply.github.com>
2019-06-11 18:31:52 -0400

    next thing to work on is to dispatch

--- app/controllers/api/v1/projects_controller.rb
+++ app/controllers/api/v1/projects_controller.rb
@@ -10,8 +10,13 @@ class Api::V1::ProjectsController < ApplicationController
     day = params[:date][3..4]
     year = params[:date][6..9]
     formatted_date = "#{year}-#{month}-#{day}"
-    new_project = Project.create(title: params[:title], description: params[:description], due_date: formatted_date, percentage: 0)
-    render json: new_project
+    new_project = Project.new(title: params[:title], description: params[:description], due_date: formatted_date, percentage: 0)
+    if new_project.save
+      UserProject.create(user_id: params[:user_id], project_id: new_project.id)
+      render json: new_project
+    else
+      render json: {error: "Please fill out all input fields"}
+    end
   end
 
 end
