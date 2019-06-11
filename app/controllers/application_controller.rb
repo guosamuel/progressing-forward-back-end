4540cd22810a7260e558cac81ff9542a52edd8cf
Samuel Guo <47992471+guosamuel@users.noreply.github.com>
2019-06-11 10:44:22 -0400

    Added authentication to render tasks respective to the user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,31 +5,30 @@ class ApplicationController < ActionController::API #requests can come from anyw
     JWT.encode({user_id: user_id}, ENV['SECRET_TOKEN'])
   end
 
-  # def auth_headers
-  #   request.headers['Authorization']
-  # end
-  #
-  # def decoded_token
-  #   begin
-  #     JWT.decode(token, ENV['SECRET_TOKEN'])
-  #   rescue
-  #     nil
-  #   end
-  # end
-  #
-  # def current_user
-  #   begin
-  #     byebug
-  #     user_id = decoded_token[0]["user_id"]
-  #     User.find(user_id)
-  #   rescue
-  #     nil
-  #   end
-  # end
-  #
-  # def logged_in
-  #   !!current_user
-  # end
+  def token
+    request.headers['Authorization']
+  end
+
+  def decoded_token
+    begin
+      JWT.decode(token, ENV['SECRET_TOKEN'])
+    rescue JTWT::DecodeError
+      nil
+    end
+  end
+
+  def current_user
+    begin
+      user_id = decoded_token[0]["user_id"]
+      User.find(user_id)
+    rescue
+      nil
+    end
+  end
+
+  def logged_in
+    !!current_user
+  end
   #
   # def authorized
   #   render json: {error: "Unauthorized"} unless logged_in
