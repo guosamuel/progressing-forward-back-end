3c76abd51dde91e3d82a6d6e3c5c9cf5394d022d
github username <github email address>
2019-06-11 22:44:37 -0400

    optimistically and pessimistically rendered new tasks WITH max due date

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -4,5 +4,18 @@ class Api::V1::TasksController < ApplicationController
     @tasks = Task.all
     render json: @tasks
   end
-  
+
+  def create
+    month = params[:date][0..1]
+    day = params[:date][3..4]
+    year = params[:date][6..9]
+    formatted_date = "#{year}-#{month}-#{day}"
+    new_task = Task.new(project_id: params[:project_id], title: params[:title], description: params[:description], due_date: formatted_date, percentage: 0)
+    if new_task.save
+      render json: new_task
+    else
+      render json: {error: "Please fill out all input fields."}
+    end
+  end
+
 end
--- app/serializers/task_serializer.rb
+++ app/serializers/task_serializer.rb
@@ -1,3 +1,3 @@
 class TaskSerializer < ActiveModel::Serializer
-  attributes :id, :title, :description, :percentage, :due_date
+  attributes :id, :title, :description, :percentage, :due_date, :project_id
 end
