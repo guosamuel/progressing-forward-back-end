74bc5221b25e4a6ba7f1b7cc870614956c72b03b
Samuel Guo <47992471+guosamuel@users.noreply.github.com>
2019-06-12 18:23:05 -0400

    need to test out the task update on the back end

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -19,6 +19,15 @@ class Api::V1::TasksController < ApplicationController
   end
 
   def update
-  
+    month = params[:date][0..1]
+    day = params[:date][3..4]
+    year = params[:date][6..9]
+    formatted_date = "#{year}-#{month}-#{day}"
+    if Task.update(params[:task_id], title: params[:title], description: params[:description], due_date: formatted_date, percentage: params[:percentage])
+      Task.update(params[:task_id], title: params[:title], description: params[:description], due_date: formatted_date, percentage: params[:percentage])
+      updated_task = Task.find(params[:task_id])
+      render json: updated_task
+    else
+      render json: {error: "Please fill out all input fields."}
   end
 end
