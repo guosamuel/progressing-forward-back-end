0405dc91b9203363d7d572028ea2179e83888bc5
Samuel Guo <47992471+guosamuel@users.noreply.github.com>
2019-06-13 16:11:22 -0400

    installed project lead for each project

--- app/serializers/project_serializer.rb
+++ app/serializers/project_serializer.rb
@@ -1,5 +1,5 @@
 class ProjectSerializer < ActiveModel::Serializer
-  attributes :id, :title, :description, :due_date, :percentage
+  attributes :id, :title, :description, :due_date, :percentage, :project_lead_id
 
   has_many :tasks
   has_many :users
