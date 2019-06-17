2ec901fff3e634fcb228a99bc83f9cfae3596566
github username <github email address>
2019-06-17 11:54:47 -0400

    Implemented the adding collaborator feature

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      resources :users, :projects, :tasks
+      resources :users, :projects, :tasks, :user_projects
       post '/auth', to: "auth#create"
       # get '/auth', to: "auth#create"
 
