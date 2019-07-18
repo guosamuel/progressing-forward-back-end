4b541a64ffe896e45c58ecc8b8fa3b2d560ad652
Samuel Guo <47992471+guosamuel@users.noreply.github.com>
2019-07-18 19:08:35 -0400

    refactored the project progress

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -9,13 +9,7 @@ class Api::V1::TasksController < ApplicationController
     new_task = Task.new(project_id: params[:project_id], title: params[:title], description: params[:description], due_date: formatted_date, percentage: 0)
     if new_task.save
       project = Project.find(params[:project_id])
-      sum = 0
-      project.tasks.each do |task|
-        sum += task.percentage
-      end
-      average = sum/project.tasks.length
-      Project.update(project.id, percentage: average)
-      updated_project = Project.find(project.id)
+      updated_project = update_project_percentage(project)
       render json: {new_task: new_task, updated_project: ProjectSerializer.new(updated_project)}
     else
       render json: {error: "Please fill out all input fields and/or please select the date from the pop-up calender."}
@@ -26,19 +20,25 @@ class Api::V1::TasksController < ApplicationController
     if Task.update(params[:task_id], title: params[:title], description: params[:description], due_date: formatted_date, percentage: params[:percentage]).valid?
       Task.update(params[:task_id], title: params[:title], description: params[:description], due_date: formatted_date, percentage: params[:percentage])
       updated_task = Task.find(params[:task_id])
-
       project = Project.find(updated_task.project_id)
-      sum = 0
-      project.tasks.each do |task|
-        sum += task.percentage
-      end
-      average = sum/project.tasks.length
-      Project.update(project.id, percentage: average)
-      updated_project = Project.find(project.id)
-      # byebug
+      updated_project = update_project_percentage(project)
       render json: {updated_task: updated_task, updated_project: ProjectSerializer.new(updated_project), success: "You've successfully updated the task."}
     else
       render json: {error: "Please fill out all input fields and/or please select the date from the pop-up calender."}
     end
   end
+
+  private
+
+  def update_project_percentage(selected_project)
+    sum = 0
+    selected_project.tasks.each do |task|
+      sum += task.percentage
+    end
+    average = sum/selected_project.tasks.length
+    Project.update(selected_project.id, percentage: average)
+    updated_project = Project.find(selected_project.id)
+    return updated_project
+  end
+
 end
