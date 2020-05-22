69d29ad8111cce657d5c65629ca96afb2707b2b2
Samuel Guo <47992471+guosamuel@users.noreply.github.com>
2020-05-22 15:23:37 -0400

    updated CORS policy to reflect Netlify changes from April

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,8 +7,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    # origins 'https://progressingforward.netlify.com'
-    origins '*'
+    origins 'https://progressingforward.netlify.app/'
+    # origins '*'
 
     resource '*',
       headers: :any,
