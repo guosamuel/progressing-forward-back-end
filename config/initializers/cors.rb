50468e9eb7df384e77b8cadec7452c7e1022fe95
Samuel Guo <47992471+guosamuel@users.noreply.github.com>
2020-05-22 15:25:47 -0400

    changed Netlify link

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://progressingforward.netlify.app/'
+    origins 'https://progressingforward.netlify.app'
     # origins '*'
 
     resource '*',
